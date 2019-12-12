961a09c35a8f73737f11c754992dfe73f2632607
RBLeon <57948143+RBLeon@users.noreply.github.com>
2019-12-12 13:00:08 +0100

    Added Leveltracker to mainfile

--- classes.py
+++ classes.py
@@ -30,20 +30,35 @@ class Game:
                 else:
                     self.playersTurn = self.playersList[0]
                     break
-    # Get a random question from an csv file with all the questions
+
+# Import all questions and answers and sort them by difficulty 
+    def importKaarten(naam):
+        with open(naam+'.csv') as kaarten:
+            kaarten = csv.reader(kaarten, delimiter=',')
+            return list(kaarten)        
+    list_makkelijk = importKaarten('import_csv/Leveltracker/MakkelijkKaarten')
+    list_gemiddeld = importKaarten('import_csv/Leveltracker/GemiddeldKaarten')
+    list_moeilijk = importKaarten('import_csv/Leveltracker/MoeilijkKaarten')
+
+# Return a question based on the players' level
     def getVraag(self):
-        with open('./import_csv/Vraagkaarten.csv') as vraagKaarten:
-            vraagKaarten = csv.reader(vraagKaarten, delimiter=',')
-            list_questions = list(vraagKaarten)
-            self.playersTurn.lastQuestion = list_questions[random.randint(0, len(list_questions) - 1)]
-            return self.playersTurn.lastQuestion
-        
+        if self.playerturn.difficulty == 'makkelijk':
+            x = list_makkelijk
+        elif self.playerturn.difficulty == 'gemiddeld':
+            x = list_gemiddeld
+        else:
+            x = list_moeilijk
+        self.playersTurn.lastQuestion = x[random.randint(0, len(x)-1)]
+        return self.playersTurn.lastQuestion
+
+# Get a random card from an csv file      
     def getDoom(self):
         with open('./import_csv/DoomKaarten.csv') as doomKaarten:
             doomKaarten = csv.reader(doomKaarten, delimiter=',')
             list_cards = list(doomKaarten)
             return str(list_cards[random.randint(0 , len(list_cards) - 1)]).strip("[]'")
         
+# Get a random card from an csv file    
     def getKans(self):
         with open('./import_csv/KansKaarten.csv') as kansKaarten:
             kansKaarten = csv.reader(kansKaarten, delimiter=',')
